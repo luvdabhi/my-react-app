b926cde478ce5aa350a865cd5756f2adcbbc82a9
Luv Dabhi <146580963+luvdabhi@users.noreply.github.com>
2023-12-03 21:19:30 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-react-app/',
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
 })
